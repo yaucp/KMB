56e63e9d3b1f43b9c02c2a4ed518913ec56a14eb
Thomas Yau <yaucpthomas1234@gmail.com>
2021-06-29 10:18:35 +0800

    optimise program and reduced few memory usage

--- reinsertETA.py
+++ reinsertETA.py
@@ -5,19 +5,12 @@ import logging
 import grequests
 
 
-# Function for changing list resp into dictionary with the bus route's direction and sequence stop number as key
-def parseRouteETA(resp):
-    result = {}
-    for etaData in resp:
-        result.setdefault((etaData['dir'], etaData['seq']), []).append(etaData)
-    return result
-
-
 def updateETA():
     RSdata = []
     etaURL = []
     etaData = {}
     try:
+        # Get all route-stop data to get info on route, stop for query link
         with arcpy.da.SearchCursor("GDB/KMB.gdb/RouteStop",
                                    ('route', 'bound', 'service_type', 'seq', 'stop',
                                     'SHAPE@XY', 'name_en', 'name_tc', 'name_sc', 'orig_en',
@@ -27,9 +20,8 @@ def updateETA():
 
         # Base url link for data extraction
         kmbETA_url = "https://data.etabus.gov.hk/v1/transport/kmb/route-eta/"
-        # Used as current indicator and stored route NO. and service type
-        currQ = None
 
+        # Get all responses from all the url of every possible route
         for RS in RSdata:
             tempURL = kmbETA_url + r"{}/{}".format(RS[0], RS[2])
             if tempURL not in etaURL:
@@ -38,13 +30,14 @@ def updateETA():
         requests = (grequests.get(u) for u in etaURL)
         responses = grequests.map(requests)
 
+        # Loop through responses and json data to make route and service_type as their key in etaData var.
         for r in responses:
             resp_data = json.loads(r.text)['data']
-            if resp_data != []:
+            # If eta data exists, can extract keys from json data
+            # Or else, parse the url in the response object as the key
+            if resp_data:
                 idx = (resp_data[0]['route'], str(resp_data[0]['service_type']))
-            else:
-                idx = (r.url[54:r.url.find("/", 55)], r.url[r.url.find("/", 55) + 1:])
-            etaData[idx] = parseRouteETA(resp_data)
+                etaData[idx] = parseRouteETA(resp_data)
 
         arcpy.management.TruncateTable("GDB/KMB.gdb/ETA/")
         field = (
@@ -53,28 +46,27 @@ def updateETA():
             'dest_tc', 'dest_sc', 'dest_en', 'eta_seq',
             'eta', 'rmk_tc', 'rmk_sc', 'rmk_en', 'timestamp')
 
-        iCursor = arcpy.da.InsertCursor("GDB/KMB.gdb/ETA", field)
+        with arcpy.da.InsertCursor("GDB/KMB.gdb/ETA", field) as iCursor:
 
-        # Start extraction and importing data according to each row of RSdata
-        for RS in RSdata:
-            data = etaData[(RS[0], RS[2])]
-            if (RS[1], RS[3]) not in data:
-                # Write None data with current row of RSdata appended
-                row = list(RS) + [None] * 9
-                iCursor.insertRow(row)
-                # for i in range(total_etaseq):
-                #     iCursor.insertRow(row)
-            else:
-                # Insert row with ETA data
-                for eta in data[(RS[1], RS[3])]:
-                    row = list(RS) + list(eta.values())[5:]
+            # Start extraction and importing data according to each row of RSdata
+            for RS in RSdata:
+                if (RS[0], RS[2]) not in etaData or (RS[1], RS[3]) not in etaData[(RS[0], RS[2])]:
+                    # Write None data with current row of RSdata appended
+                    row = list(RS) + [None] * 9
                     iCursor.insertRow(row)
-                # insert row when number of inserted row is less that total_etaseq
-                # if total_etaseq != 1:
-                #     for i in range(total_etaseq):
-                #         row = list(RS) + [None] * 6
-                #         iCursor.insertRow(row)
-        del iCursor
+                    # for i in range(total_etaseq):
+                    #     iCursor.insertRow(row)
+                else:
+                    # Insert row with ETA data
+                    data = etaData[(RS[0], RS[2])]
+                    for eta in data[(RS[1], RS[3])]:
+                        row = list(RS) + list(eta.values())[5:]
+                        iCursor.insertRow(row)
+                    # insert row when number of inserted row is less that total_etaseq
+                    # if total_etaseq != 1:
+                    #     for i in range(total_etaseq):
+                    #         row = list(RS) + [None] * 6
+                    #         iCursor.insertRow(row)
 
     except Exception as inst:
         print(inst)
@@ -88,8 +80,7 @@ def updateETA():
 def parseRouteETA(resp):
     result = {}
     for etaData in resp:
-        result.setdefault((etaData['dir'], etaData['seq']), []).append(
-            etaData)
+        result.setdefault((etaData['dir'], etaData['seq']), []).append(etaData)
     return result
 
 
